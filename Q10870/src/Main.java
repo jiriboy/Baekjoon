d800f1adc2fc90e5c51fc918868bd5e168c7952b
jiriboy <jsp13@snu.ac.kr>
2020-10-18 03:27:37 +0900

    Solved 10870

---
+++ Q10870/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        int[] fib = new int[n + 1];
+
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        System.out.println(fib[n]);
+    }
+}
