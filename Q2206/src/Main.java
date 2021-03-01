459db0f07a0af0a62d15e613233dc4867880939a
jiriboy <jsp13@snu.ac.kr>
2021-03-01 14:32:22 +0900

    Solved 2206

---
+++ Q2206/src/Main.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        char[][] map = new char[N][M];
+        int[][][] shortest = new int[N][M][2];
+
+        for (int i = 0; i < N; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+        LinkedList<State> queue = new LinkedList<>();
+        queue.add(new State(0, 0, 0, 1));
+        shortest[0][0][0] = 1;
+
+        int answer = -1;
+
+        while (!queue.isEmpty()) {
+            State s = queue.poll();
+
+            if (s.x == N - 1 && s.y == M - 1) {
+                answer = s.d;
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int x = s.x + dx[i];
+                int y = s.y + dy[i];
+
+                if (x < 0 || x >= N || y < 0 || y >= M) {
+                    continue;
+                }
+
+                if ((shortest[x][y][s.w] == 0 || shortest[x][y][s.w] > s.d + 1) && map[x][y] == '0') {
+                    shortest[x][y][s.w] = s.d + 1;
+                    queue.add(new State(x, y, s.w, s.d + 1));
+                }
+
+                if (s.w == 0 && (shortest[x][y][0] == 0 || shortest[x][y][0] > s.d + 1) && map[x][y] == '1') {
+                    shortest[x][y][1] = s.d + 1;
+                    queue.add(new State(x, y, 1, s.d + 1));
+                }
+            }
+        }
+
+        bw.write(Integer.toString(answer));
+        bw.close();
+        br.close();
+    }
+
+    static class State {
+        int x;
+        int y;
+        int w;
+        int d;
+
+        State(int x, int y, int w, int d) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.d = d;
+        }
+    }
+}
